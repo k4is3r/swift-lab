15afa31f24a5ef3187d68019868ad21c16f47121
Eduardo Imery <ejwimery@gmail.com>
2019-10-23 14:54:53 -0600

    class 7 - clousures

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,21 +1,39 @@
 import Foundation
 
+extension Date {
+    init(year:Int, month:Int, day:Int){
+        let calendar =  Calendar(identifier: .gregorian)
+        var dateComponents = DateComponents()
+        dateComponents.year = year
+        dateComponents.day = day
+        dateComponents.month = month
+        self = calendar.date(from: dateComponents) ?? Date()
+    }
+}
+
 protocol InvalidateTransaction {
     func invalidateTransaction(transaction: Transaction)
 }
+
+typealias TransactionHandler = ( (_ completed: Bool, _ confirmation: Date) -> Void )
 
 protocol Transaction {
     var value: Float { get }
     var name: String { get }
     var isValid: Bool { get set }
     var delegate: InvalidateTransaction? {get set}
+    var date: Date { get }
+    var handler: TransactionHandler? { get set }
+    var completed: Bool { get }
+    var confirmation: Date? {get}
 }
 
 extension Transaction {
     mutating func invalidateTransaction(){
-        isValid = false
-
-        delegate?.invalidateTransaction(transaction: self)
+        if completed {
+            isValid = false
+            delegate?.invalidateTransaction(transaction: self)
+        }
     }
 
 }
@@ -30,33 +48,51 @@ enum DebitCategories: Int{
 }
 
 enum TransactionType {
-    case debit(value: Float, name: String, category: DebitCategories)
-    case gain(value: Float, name: String)
+    case debit(value: Float, name: String, category: DebitCategories, date: Date)
+    case gain(value: Float, name: String, date: Date)
 }
 
 class Debit: TransactionDebit{
+    var date: Date
     var delegate: InvalidateTransaction?
     var value : Float
     var name: String
     var category: DebitCategories
     var isValid: Bool = true
-    init(value: Float, name: String, category:DebitCategories) {
+    var handler: TransactionHandler?
+    var completed: Bool = false
+    var confirmation: Date?
+    init(value: Float, name: String, category:DebitCategories, date: Date) {
         self.category = category
         self.value = value
         self.name = name
+        self.date = date
+        DispatchQueue.main.asyncAfter(deadline: .now()){
+            self.handler?(true, Date())
+            print("Confirmed transaction", Date())
+        }
     }
     
 }
 
 class Gain: Transaction{
+    var date: Date
     var delegate: InvalidateTransaction?
     var value: Float
     var name: String
     var isValid : Bool = true
-    
-    init(value: Float, name: String){
+    var handler: TransactionHandler?
+    var completed: Bool = false
+    var confirmation: Date?
+    init(value: Float, name: String, date: Date){
         self.value = value
         self.name = name
+        self.date = date
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1){
+            self.handler?(true, Date())
+            print("Confirmed transaction", Date())
+        }
     }
     
 }
@@ -82,22 +118,30 @@ class Account {
     @discardableResult
     func addTransaction( transaction: TransactionType) -> Transaction? {
         switch transaction {
-        case .debit(let value, let name, let category):
+        case .debit(let value, let name, let category, let date):
             if (amount - value) < 0{
                 return nil
             }
-            let debit = Debit(value: value, name: name, category: category)
+            let debit = Debit(value: value, name: name, category: category, date: date)
             debit.delegate = self
-            amount -= debit.value
-            transactions.append(debit)
-            debits.append(debit)
+            
+            debit.handler = { (completed, confirmation) in
+                debit.confirmation = confirmation
+                self.amount -= debit.value
+                self.transactions.append(debit)
+                self.debits.append(debit)
+            }
             return debit
-        case .gain(let value, let name):
-            let gain = Gain(value:value, name:name)
+        case .gain(let value, let name, let date):
+            let gain = Gain(value:value, name:name, date: date)
             gain.delegate = self
-            amount += gain.value
-            transactions.append(gain)
-            gains.append(gain)
+            gain.handler = { (completed, confirmation) in
+                gain.confirmation = confirmation
+                self.amount += gain.value
+                self.transactions.append(gain)
+                self.gains.append(gain)
+            }
+
             return gain
         }
         
@@ -162,7 +206,8 @@ me.account?.addTransaction(
     transaction: .debit(
         value: 20,
         name: "Cafe con amigos",
-        category:DebitCategories.food
+        category:DebitCategories.food,
+        date: Date(year: 2019, month: 11, day: 10)
     )
 )
 
@@ -170,7 +215,8 @@ me.account?.addTransaction(
     transaction: .debit(
         value: 100,
         name: "Juego PS4",
-        category:.entretaining
+        category:.entretaining,
+        date: Date(year: 2019, month: 11, day: 13)
     )
 )
 
@@ -178,46 +224,65 @@ me.account?.addTransaction(
     transaction: .debit(
         value: 3400,
         name: "MacbookPro",
-        category:.entretaining
+        category:.entretaining,
+        date: Date(year: 2019, month: 11, day: 14)
     )
 )
 
 me.account?.addTransaction(
     transaction: .gain(
         value: 1200,
-        name: "Rembolso compra"
+        name: "Rembolso compra",
+        date: Date(year: 2019, month: 11, day: 17)
     )
 )
 
 me.account?.addTransaction(
     transaction: .gain(
         value: 1200,
-        name: "Salario"
+        name: "Salario",
+        date: Date(year: 2019, month: 11, day: 19)
     )
 )
 
 var salary = me.account?.addTransaction(
     transaction:.gain(
         value: 1200,
-        name: "Salario"
+        name: "Salario",
+        date: Date(year: 2019, month: 11, day: 21)
     )
 )
 
-salary?.invalidateTransaction()
+DispatchQueue.main.asyncAfter(deadline: .now() + 1){
+    salary?.invalidateTransaction()
+    print("Invalidated")
+}
 
 print(me.account!.amount)
 print(me.fullName)
-let transactions = me.account?.transactionsFor(category: .entretaining) as? [Debit]
-for transaction in transactions ?? []{
-     print(
-        transaction.name,
-        transaction.value,
-        transaction.category.rawValue
-    )
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 2){
+    let transactions = me.account?.transactionsFor(category: .entretaining) as? [Debit]
+    for transaction in transactions ?? []{
+        print(
+            "Hello, ",
+            transaction.name,
+            transaction.value,
+            transaction.category.rawValue,
+            transaction.date
+        )
+    }
+    
+    for gain in me.account?.gains ?? [] {
+        print(
+            "Hello gain,",
+            gain.name,
+            gain.isValid,
+            gain.value,
+            gain.date
+        )
+    }
 }
 
-for gain in me.account?.gains ?? [] {
-    print(gain.name, gain.isValid, gain.value)
-}
 
 print(me.account?.amount ?? 0)
